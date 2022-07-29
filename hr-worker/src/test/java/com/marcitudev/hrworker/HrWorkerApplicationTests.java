8c28ec9a62e27dd462b9936df9446e26675a53c8
Márcio Costa <marcio.costa@virtus.ufcg.edu.br>
2022-07-28 22:35:22 -0300

    Project hr-worker created

---
+++ hr-worker/src/test/java/com/marcitudev/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcitudev.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
